c09f4df85be96305b01cd343f895620659090be7
Thomas F. Wolobah Jr <twolobah04@gmail.com>
2023-12-28 22:30:10 +0530

    added a new folder called Recursion

---
+++ DSA_Practices/DSA/Recursion/TowerOfHanoi.java
@@ -0,0 +1,16 @@
+package DSA.Recursion;
+
+public class TowerOfHanoi {
+    public static void main(String[] args){
+        towerOfHanoi(3, 'A', 'B', 'C');
+    }
+    static void towerOfHanoi(int n, char src, char aux, char dest){
+        if(n == 1){
+            System.out.println(src + " --> " + dest);
+            return;
+        }
+        towerOfHanoi(n - 1, src, dest, aux);
+        towerOfHanoi(1, src, aux, dest);
+        towerOfHanoi(n - 1, aux, src, dest);
+    }
+}
