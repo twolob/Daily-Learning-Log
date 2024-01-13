06a89875f1c0f9866b36526ee5e65cd62e5eec6b
Thomas F. Wolobah Jr <twolobah04@gmail.com>
2024-01-13 23:09:21 +0530

    the Search_In_Rotated_Sorted_Array.java file was modified

--- DSA_Practices/DSA/Binary_Search/Search_In_Rotated_Sorted_Array.java
+++ DSA_Practices/DSA/Binary_Search/Search_In_Rotated_Sorted_Array.java
@@ -2,16 +2,18 @@ package DSA.Binary_Search;
 
 public class Search_In_Rotated_Sorted_Array {
     public static void main(String[] args){
-        int[] nums = {4,5,6,7,0,1,2};
-        int target = 0;
+        int[] nums = {4,5,6,7,8,0,1,2};
+        int target = 6;
         int answer = search(nums, target);
         System.out.println(answer);
     }
     static int search(int[] nums, int target){
         int pivot = checkRotatingArray(nums);
-        int half = searchHalf(nums, target, 0, pivot);
-
-        if(half != -1){
+        int half = searchHalf(nums, target, 0, pivot - 1);
+        if (nums[pivot] == target){
+            return pivot;
+        }
+        if(target >= nums[0]){
             return half;
         }
         return searchHalf(nums, target, pivot + 1, nums.length - 1);
@@ -19,12 +21,13 @@ public class Search_In_Rotated_Sorted_Array {
     static int checkRotatingArray(int[] nums){
         int start = 0;
         int end = nums.length - 1;
+
         while(start <= end){
             int mid = start + (end - start) / 2;
             if(mid < end && nums[mid] > nums[mid + 1]){
                 return mid;
             }
-            if (mid > end && nums[mid] < nums[mid - 1]){
+            if (mid > start && nums[mid] < nums[mid - 1]){
                 return mid - 1;
             }
             else if(nums[mid] <= nums[start]){
@@ -39,6 +42,7 @@ public class Search_In_Rotated_Sorted_Array {
     static int searchHalf(int[] nums, int target, int start, int end){
         while(start <= end){
             int mid = start + (end - start) / 2;
+
             if(target > nums[mid]){
                 start = mid + 1;
             }
@@ -46,7 +50,7 @@ public class Search_In_Rotated_Sorted_Array {
                 end = mid - 1;
             }
             else{
-                return -1;
+                return mid;
             }
         }
         return -1;
