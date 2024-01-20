cfe7f8f7d8c812e0d501e762765e92223052af7b
Thomas F. Wolobah Jr <twolobah04@gmail.com>
2024-01-20 23:50:38 +0530

    Added new folder called Sorting with BobbleSort.java in it

---
+++ DSA_Practices/DSA/Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+package DSA.Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args){
+        int[] arr = {8, 4, 1, 6, 7, 3, 4, 2};
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        boolean isSorted;
+        for(int i = 0; i < arr.length; i++){
+            isSorted = true;
+            for(int j = 1; j < arr.length - i; j++){
+                if(arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if(isSorted){
+               break;
+            }
+        }
+    }
+}
