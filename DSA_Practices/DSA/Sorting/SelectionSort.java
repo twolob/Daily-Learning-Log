232cee302f92fa96050de897bc6437cef15146a8
Thomas F. Wolobah Jr <twolobah04@gmail.com>
2024-01-29 14:45:40 +0530

    The SelectionSort.java was modified

--- DSA_Practices/DSA/Sorting/SelectionSort.java
+++ DSA_Practices/DSA/Sorting/SelectionSort.java
@@ -1,22 +1,31 @@
 package DSA.Sorting;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args){
-        int[] arr = {3, 6, 1, 8, 2, 9};
+        int[] arr = {1, 6, 1, 8, 2, 78, 22, 0, 9, 93};
+        selection(arr);
     }
     static void selection(int[] arr){
         for(int i = 0; i < arr.length; i++){
-            int count = -1;
-            int smallest = arr[0];
+            int smallest = arr[i];
+            int count = i;
             for(int j = i; j < arr.length; j++){
                 if(smallest > arr[j]){
                     smallest = arr[j];
+                    count = j;
                 }
             }
-
+            //Swap
+            swap(arr, i, count);
+            count = 0;
         }
+        System.out.println(Arrays.toString(arr));
     }
-    static void swap(int smallest, int[] arr){
-        int temp;
+    static void swap(int[] arr, int i, int count){
+        int temp = arr[count];
+        arr[count] = arr[i];
+        arr[i] = temp;
     }
 }
